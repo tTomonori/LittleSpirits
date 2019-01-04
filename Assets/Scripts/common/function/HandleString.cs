7868304f6dba2be1ed4ccb970862fb13bd9a7baa
tomonori takada <tomo@tomonorinoMacBook-Pro.local>
2019-01-04 17:53:47 +0900

    UML更新

--- Assets/Scripts/common/function/HandleString.cs
+++ Assets/Scripts/common/function/HandleString.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class HandleString {
+    ///指定文字列中の最後のseparatorより前の部分を抽出
     public static string cutOff(string aString,string aSeparator){
         int tIndex = aString.LastIndexOf(aSeparator);
         if (tIndex < 0) { return ""; }
